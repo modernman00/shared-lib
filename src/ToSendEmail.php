5e4356d3b5916aa2afd184336e2daae9aac9d226
modernman00 <modernman00@yahoo.com>
2025-07-17 19:42:58 +0100

    csl

--- src/ToSendEmail.php
+++ src/ToSendEmail.php
@@ -33,16 +33,16 @@ class ToSendEmail
 
   public static function sendEmailGeneral($array, $recipient)
   {
-    
-  try {
 
-    if (!defined('PASS')) {
-      EmailData::defineConstants($recipient, $_ENV);
-      // if it is still not set, then throw an error
+    try {
+
       if (!defined('PASS')) {
-        throw new ForbiddenException('Email credentials (constant) not set');
+        EmailData::defineConstants($recipient, $_ENV);
+        // if it is still not set, then throw an error
+        if (!defined('PASS')) {
+          throw new ForbiddenException('Email credentials (constant) not set');
+        }
       }
-    }
       $data = $array['data'];
 
       ob_start();
@@ -51,43 +51,47 @@ class ToSendEmail
       $emailContent = ob_get_clean() ?: throw new ForbiddenException('Failed to render email content');
 
       // Emogrify the HTML for email client compatibility
-            $cssInliner = CssInliner::fromHtml($emailContent)->inlineCss();
-            $domDocument = $cssInliner->getDomDocument();
-            HtmlPruner::fromDomDocument($domDocument)
-                ->removeElementsWithDisplayNone()
-                ->removeRedundantClassesAfterCssInlined($cssInliner);
-            $converter = CssToAttributeConverter::fromDomDocument($domDocument)
-                ->convertCssToVisualAttributes();
-            $emogrifiedContent = $converter->render();
+      $cssInliner = CssInliner::fromHtml($emailContent)->inlineCss();
+      $domDocument = $cssInliner->getDomDocument();
+      HtmlPruner::fromDomDocument($domDocument)
+        ->removeElementsWithDisplayNone()
+        ->removeRedundantClassesAfterCssInlined($cssInliner);
+      $converter = CssToAttributeConverter::fromDomDocument($domDocument)
+        ->convertCssToVisualAttributes();
+      $emogrifiedContent = $converter->render();
 
       ob_end_clean();
 
-     // Determine email recipient
-            $email = Utility::checkInputEmail($data['email'] ?? $array['email'] ?? '');
-            if (empty($email)) {
-                throw new InvalidArgumentException('Email address is required');
-            }
+      // Determine email recipient
+      $email = Utility::checkInputEmail($data['email'] ?? $array['email'] ?? '');
+      if (empty($email)) {
+        throw new InvalidArgumentException('Email address is required');
+      }
 
-  
-            // check if there is $data['name'] or $array['name']
-            $name = $array['name'] ?? '';
+      // check if $data['name'] or $array['name'] is set
+      if (isset($data['name'])) {
+        $name = $data['name'];
+      } else {
+        $name = $array['name'];
+      }
+
+      $name = Utility::cleanSession($name);
+      // check if there is $data['name'] or $array['name']
+      $name = $name ?? 'there';
 
 
-      $name = Utility::cleanSession($data['name']) ?? Utility::cleanSession($array['name']) ?? "";
+
 
       SendEmail::sendEmail($email, $name, $array['subject'], $emailContent);
-      } catch (ForbiddenException $e) {
-            Utility::showError($e);
-           
-        } catch (InvalidArgumentException $e) {
-            Utility::showError($e);
-           
-        } catch (\Exception $e) {
-            Utility::showError($e);
-    
-        }
+    } catch (ForbiddenException $e) {
+      Utility::showError($e);
+    } catch (InvalidArgumentException $e) {
+      Utility::showError($e);
+    } catch (\Exception $e) {
+      Utility::showError($e);
     }
-  
+  }
+
 
   /**
    * You have to generate the $var using the genEmailArray()
@@ -103,7 +107,7 @@ class ToSendEmail
 
 
     if (!defined('PASS')) {
-         EmailData::defineConstants($recipientType, $_ENV);
+      EmailData::defineConstants($recipientType, $_ENV);
     }
 
     $data = $var['data'];
